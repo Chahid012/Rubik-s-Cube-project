58eb5b01ac046f487ee01835b995c6086f5fc8b2
Chahid <147660895+Chahid012@users.noreply.github.com>
2024-03-08 05:00:52 +0100

    Create khaibao.h
    servo motor

---
+++ khaibao.h
@@ -0,0 +1,20 @@
+
+int gan1b = 175;
+int gan2b = 175;
+int gan3b = 175;
+int gan4b = 175;
+
+int xa1b = 40;
+int xa2b = 50;
+int xa3b = 40;
+int xa4b = 40;
+
+int dung1a = 145;
+int dung2a = 22;
+int dung3a = 120;
+int dung4a = 33;
+
+int ngang1a = 47;
+int ngang2a = 117;
+int ngang3a = 23;
+int ngang4a = 127;
